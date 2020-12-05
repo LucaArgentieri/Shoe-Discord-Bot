bd8b8ec819c196ea7c16bdc0abe1a968e6c04699
giannandreanobu <75544118+giannandreanobu@users.noreply.github.com>
2020-12-05 22:07:41 +0100

    update
    - almeno ora scrive nel file json

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const fs = require('fs');
+
 bot.login(process.env.DISCORD_KEY);
+var path = require('path');
+var filename = path.basename('C:\Users\Nothingbeatzz\Documents\GitHub\DiscordBot\JsonList.json');
+global.fetch = require("node-fetch");
+fetch(filename)
+.then(res => res.json())
+.then(data => console.log(data))
+
+
 //fz link
 const a = {
     link:[]
@@ -22,9 +31,10 @@ bot.on('message', (msg) => {        //add Raffle links
     if (msg.content.substr(0, 9) === "!addlinks") {
         const x = msg.content.substr(10, msg.length);
         console.log(a.link);
-        a.link.push(x)
-        let data = JSON.stringify(a);
-        fs.writeFileSync('JsonList.json', data);
+        a.link.push(x);
+        let data = JSON.stringify(a.link);
+        fs.appendFileSync('JsonList.json', data, "UTF-8",{'flags': 'a+'});
+        // fs.appendFile('JsonList.json', data);
         msg.reply("Link aggiunto con successo");
     }
 });
@@ -35,7 +45,7 @@ console.log(a);
 
 bot.on('message', (msg) => {        //Elenco lista Raffle
     if (msg.content == '!Raffle') {
-        msg.reply(lista);
+        msg.reply(a.link);
     }
 });
 bot.on('message', (message) => {  //chat
