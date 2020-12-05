63b702cc69c3231eccf0ddd2256e428543553432
giannandreanobu <75544118+giannandreanobu@users.noreply.github.com>
2020-12-05 20:54:10 +0100

    scheletro
    xd

---
+++ DiscordDropTrack_BOT/index.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const fs = require('fs');
+bot.login('Nzg0NzgzNTc3NzE1NDQxNjY1.X8uUqQ.UMpJH-FoWnm8hWDgE7BzmTcpLN0');
+//fz link
+const a = {
+    link:[]
+}; 
+
+bot.on('message', (msg) => {        //Presentati
+    if (msg.content == '!presentati') {
+        msg.channel.send('AO mo che so qui te aggiorno sulle paia, basta che me lo chiedi, scrivi !help per conoscere piu comandi *wink wink* :wink::wink::wink:  ');
+    }
+});
+bot.on('message', (msg) => {        //Help
+    if (msg.content == '!help') {
+        msg.channel.send('Ciao i comandi disponibili per questo bot sono: \n !presentati - non lo sappiamo nemmeno noi, provalo.');
+    }
+});
+bot.on('message', (msg) => {        //add Raffle links      
+    if (msg.content.substr(0, 9) === "!addlinks") {
+        let data = JSON.stringify(a);
+        const x = msg.content.substr(10, msg.length);
+        console.log(a.link);
+        a.link.push(x)
+        fs.writeFileSync('JsonList.json', data);
+        msg.reply("Link aggiunto con successo");
+    }
+});
+console.log(a);
+
+
+
+
+bot.on('message', (msg) => {        //Elenco lista Raffle
+    if (msg.content == '!Raffle') {
+        msg.reply(lista);
+    }
+});
+bot.on('message', (message) => {  //chat
+    console.log(`[${message.author.tag}]: ${message.content}`)
+});
