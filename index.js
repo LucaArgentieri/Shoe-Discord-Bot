4f50387f430d4b3c9e60309c617b3d33573505b6
giannandreanobu <75544118+giannandreanobu@users.noreply.github.com>
2020-12-07 15:49:34 +0100

    Txt file
    Lista non piu in Json

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 require('dotenv').config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const fs = require('fs');
+var fs = require('fs');
 
 bot.login(process.env.DISCORD_KEY);
-var path = require('path');
-var filename = path.basename('C:\Users\Nothingbeatzz\Documents\GitHub\DiscordBot\JsonList.json');
-global.fetch = require("node-fetch");
-fetch(filename)
-.then(res => res.json())
-.then(data => console.log(data))
-
-
-//fz link
-const a = {
-    link:[]
-}; 
-
 bot.on('message', (msg) => {        //Presentati
-    if (msg.content == '!presentati') {
-        msg.channel.send('AO mo che so qui te aggiorno sulle paia, basta che me lo chiedi, scrivi !help per conoscere piu comandi *wink wink* :wink::wink::wink:  ');
+    if (msg.content.toLowerCase() == '!ciao') {
+        msg.reply('AO mo che so qui te aggiorno sulle paia, basta che me lo chiedi, scrivi !help per conoscere piu comandi *wink wink* :wink::wink::wink:  ');
     }
 });
 bot.on('message', (msg) => {        //Help
-    if (msg.content == '!help') {
-        msg.channel.send('Ciao i comandi disponibili per questo bot sono: \n !presentati - non lo sappiamo nemmeno noi, provalo.');
+    if (msg.content.toLowerCase() == '!help') {
+        msg.reply('Ciao i comandi disponibili per questo bot sono: \n !presentati - non lo sappiamo nemmeno noi, provalo.');
     }
 });
 bot.on('message', (msg) => {        //add Raffle links      
-    if (msg.content.substr(0, 9) === "!addlinks") {
-        const x = msg.content.substr(10, msg.length);
-        console.log(a.link);
-        a.link.push(x);
-        let data = JSON.stringify(a.link);
-        fs.appendFileSync('JsonList.json', data, "UTF-8",{'flags': 'a+'});
-        // fs.appendFile('JsonList.json', data);
-        msg.reply("Link aggiunto con successo");
+    if (msg.content.substr(0, 9).toLowerCase() === "!addlinks") {
+        var arraylist = fs.readFileSync('Links.txt', 'utf8').split('\n');
+        console.log(arraylist.length);
+        if (arraylist.length === 1) {
+            const x = msg.content.substr(10, msg.length);
+            fs.writeFile('Links.txt', x + "\r\n", { flag: 'r+' }, err => { });
+            msg.reply("done")
+        }
+        else{
+            const x = msg.content.substr(10, msg.length);
+            let data = arraylist + x;
+            fs.writeFile('Links.txt', data, { flag: 'r+' }, err => { });
+            msg.reply("done")
+            
+        }
+
     }
 });
-console.log(a);
 
 
 
 
 bot.on('message', (msg) => {        //Elenco lista Raffle
-    if (msg.content == '!Raffle') {
-        msg.reply(a.link);
+    if (msg.content.toLowerCase() == '!raffle') {
+        var arraylist = fs.readFileSync('Links.txt', 'utf8').split('\n');
+        msg.reply(("Ecco i link per le raffle:\r\n")+arraylist);
     }
+
 });
 bot.on('message', (message) => {  //chat
     console.log(`[${message.author.tag}]: ${message.content}`)
+});
+
+bot.on('message', (msg) => {        //Svuota lista
+    
+    if (msg.content.toLowerCase() == '!clearlist') {
+        
+        msg.reply("sicuro di voler svuotare la lista? Ormai ho fatto xd");
+    }
 });
