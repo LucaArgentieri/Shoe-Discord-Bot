d88d21d6da8283193a15d83821157c302dec4cfa
giannandreanobu <75544118+giannandreanobu@users.noreply.github.com>
2020-12-08 15:07:55 +0100

    Implementazione api StockX e altri siti di resell
    Co-Authored-By: Luca Argentieri <Lucaargentieri-98@hotmail.it>

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const SneaksAPI = require('sneaks-api');
+const sneaks = new SneaksAPI();
 const { Console } = require('console');
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 var fs = require('fs');
 
 bot.login(process.env.DISCORD_KEY);
+
+
 bot.on('message', (msg) => {        //Presentati
     if (msg.content.toLowerCase() == '!ciao') {
         msg.reply('AO mo che so qui te aggiorno sulle paia, basta che me lo chiedi, scrivi !help per conoscere piu comandi *wink wink* :wink::wink::wink:  ');
@@ -41,10 +45,6 @@ bot.on('message', (msg) => {        //add Raffle links
 
     }
 });
-
-
-
-
 bot.on('message', (msg) => {        //Elenco lista Raffle
     if (msg.content.toLowerCase() == '!raffle') {
         var arraylist = fs.readFileSync('Links.txt', 'utf8')
@@ -55,11 +55,32 @@ bot.on('message', (msg) => {        //Elenco lista Raffle
 bot.on('message', (message) => {  //chat
     console.log(`[${message.author.tag}]: ${message.content}`)
 });
-
-bot.on('message', (msg) => {        //Svuota lista
-
-    if (msg.content.toLowerCase() == '!clearlist') {
-
-        msg.reply("sicuro di voler svuotare la lista? Ormai ho fatto xd");
+bot.on('message', (msg) => {         //prezzo retail
+    if (msg.content.substr(0,7 ).toLowerCase() === "!retail") {
+        const x = msg.content.substr(7, msg.length);
+        console.log(x)
+        sneaks.getProducts(x, function(err, products){
+            let data = products; 
+            console.log(data[0]);
+            msg.reply(`Il retail di queste paia è di: ${data[0].retailPrice}$`);
+        })
     }
+
+
+});
+bot.on('message', (msg) => {         //prezzo resell
+    if (msg.content.substr(0,7 ).toLowerCase() === "!resell") {
+        const x = msg.content.substr(7, msg.length);
+        console.log(x)
+        sneaks.getProducts(x, function(err, products){
+            // console.log(data[0]);
+            // msg.reply(`Il resell delle paia ${data[0].shoeName} è di:`+"```"+`${data[0].lowestResellPrice}(valori in Dollari)`+ "```"+`${data[0].thumbnail}`);
+            products.slice(0,4).forEach(function(resell,i){
+                console.log(resell);
+            msg.reply(`Resell delle paia ${resell.shoeName} uscite a retail a `+`${resell.retailPrice}$`+ ` è di; `+"\n"+ `StockX:  ${resell.lowestResellPrice.stockX}$`+"\n"+ `FlightClub:  ${resell.lowestResellPrice.flightClub}$`+"\n"+ `Goat:  ${resell.lowestResellPrice.goat}$`+"\n"+`StadiumGoods:  ${resell.lowestResellPrice.stadiumGoods}$`+"\n"+`${resell.thumbnail}`)
+            })
+        })
+    }
+
+
 });
