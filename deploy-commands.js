2194cd81bd17cdbbca72cf6bc6e47bab77913932
LucaArgentieri <lucaargentieri-98@hotmail.it>
2024-02-15 22:10:20 +0100

    update setup

---
+++ deploy-commands.js
@@ -0,0 +1,35 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const help = require('./commands/help');
-const nextDrop = require('./commands/nextDrop');
-const infoShoesNumber = require('./commands/infoShoesNumber');
-const monthDrop = require('./commands/monthDrop');
-const mostPopular = require('./commands/mostPopular');
-const retail = require('./commands/retail');
-const todayDrop = require('./commands/todayDrop');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
-const shoeDiscordBot = () => {
-  help();
-  infoShoesNumber()
-  monthDrop();
-  mostPopular();
-  nextDrop();
-  retail();
-  todayDrop();
-};
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-shoeDiscordBot();
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(process.env.BOT_TOKEN);
